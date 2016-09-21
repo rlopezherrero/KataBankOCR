e65ab5cbefd5cdaa79ba337753bedce11a6ac7b5
Rober <Rober@192.168.1.8>
2016-09-22 00:21:44 +0200

    Creation of dummy test

---
+++ katabankOCR/src/katabankOCR/BCDDigits.java
@@ -0,0 +1,64 @@
+package katabankOCR;
+
+public class BCDDigits {
+	
+	public static String[] ZERO={
+		" _ ",
+		"| |",
+		"|_|"
+	};
+	
+	public static String[] ONE={
+			"   ",
+			"  |",
+			"  |"
+		};
+	
+	public static String[] TWO={
+			" _ ",
+			" _|",
+			"|_ "
+		};
+
+	public static String[] THREE={
+			" _ ",
+			" _|",
+			" _|"
+		};
+	
+	public static String[] FOUR={
+			"   ",
+			"|_|",
+			"  |"
+		};
+	
+	public static String[] FIVE={
+			" _ ",
+			"|_ ",
+			" _|"
+		};
+	
+	public static String[] SIX={
+			" _ ",
+			"|_ ",
+			"|_|"
+		};
+	
+	public static String[] SEVEN={
+			" _ ",
+			"  |",
+			"  |"
+		};
+	
+	public static String[] EIGHT={
+			" _ ",
+			"|_|",
+			"|_|"
+		};
+	
+	public static String[] NINE={
+			" _ ",
+			"|_|",
+			" _|"
+		};
+}
